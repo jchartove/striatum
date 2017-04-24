3e490cd0a7f74b3f9088ac48dc2c8933d0932c37
jchartove <chartove@bu.edu>
2017-04-24 14:36:46 -0400

    converted to dynasim

--- golomb_100/golomb_100_dynasim.m
+++ golomb_100/golomb_100_dynasim.m
@@ -1,20 +1,25 @@
 clear
 % Model: golomb_activedend_10
-cd '/project/crc-nak/jchartove/dynasim/golomb_100';
+%cd '/project/crc-nak/jchartove/striatum/golomb_100';
+
+eqns={
+  %'dV/dt=@current';
+	'V''=(current)./cm'
+};
 
 for numcells = [100]
 spec=[];
 T0 = 2000;
-spec.nodes(1).label = 'soma';
-spec.nodes(1).multiplicity = numcells;
-spec.nodes(1).dynamics = {'v''=current'};
-spec.nodes(1).mechanisms = {'soma_golomb_K','soma_golomb_Kdr','soma_golomb_Na','soma_input','soma_leak'};
+spec.nodes(1).name = 'soma';
+spec.nodes(1).size = numcells;
+spec.nodes(1).equations = eqns;
+spec.nodes(1).mechanism_list = {'soma_golomb_K','soma_golomb_Kdr','soma_golomb_Na','soma_input','soma_leak'};
 spec.nodes(1).parameters = {'v_IC',-90+90*rand(1,numcells), 'Tfinal', T0};
 
-spec.nodes(2).label = 'dend';
-spec.nodes(2).multiplicity = numcells;
-spec.nodes(2).dynamics = {'v''=current'};
-spec.nodes(2).mechanisms = {'dend_golomb_K','dend_golomb_Kdr','dend_golomb_Na','dend_input','dend_leak','dend_iMultiPoissonExp'};
+spec.nodes(2).name = 'dend';
+spec.nodes(2).size = numcells;
+spec.nodes(2).equations = eqns;
+spec.nodes(2).mechanism_list = {'dend_golomb_K','dend_golomb_Kdr','dend_golomb_Na','dend_input','dend_leak','dend_iMultiPoissonExp'};
 spec.nodes(2).parameters = {'v_IC',-90+90*rand(1,numcells), 'Tfinal', T0}; 
 
 ncells = 100;  % number of MSN cells in the pool
@@ -23,50 +28,50 @@ g_m = 1.2; % 1.2; % 1.3; % 1.2 parkinsonian, 1.3 normal
 %V_ic = -63;
 vrand = 63*rand(1,ncells);
 
-spec.nodes(3).label = 'D1';
-spec.nodes(3).multiplicity = ncells;
-spec.nodes(3).dynamics = {'V''=(current)./cm'};
-spec.nodes(3).mechanisms = {'naCurrentMSN','kCurrentMSN','mCurrentMSN','leakCurrentMSN','injectedCurrentD1','noisyInputMSN'};
+spec.nodes(3).name = 'D1';
+spec.nodes(3).size = ncells;
+spec.nodes(3).equations = eqns;
+spec.nodes(3).mechanism_list = {'naCurrentMSN','kCurrentMSN','mCurrentMSN','leakCurrentMSN','injectedCurrentD1','noisyInputMSN'};
 spec.nodes(3).parameters = {'cm',1,'V_IC',-63,'g_m',g_m,'Tfinal', T0}; % V_IC refers to the initial condition for the membrane 
 
-spec.nodes(4).label = 'D2';
-spec.nodes(4).multiplicity = ncells;
-spec.nodes(4).dynamics = {'V''=(current)./cm'};
-spec.nodes(4).mechanisms = {'naCurrentMSN','kCurrentMSN','mCurrentMSN','leakCurrentMSN','injectedCurrentD2','noisyInputMSN'};
+spec.nodes(4).name = 'D2';
+spec.nodes(4).size = ncells;
+spec.nodes(4).equations = eqns;
+spec.nodes(4).mechanism_list = {'naCurrentMSN','kCurrentMSN','mCurrentMSN','leakCurrentMSN','injectedCurrentD2','noisyInputMSN'};
 spec.nodes(4).parameters = {'cm',1,'V_IC',-63,'g_m',g_m,'Tfinal', T0}; % V_IC refers to the initial condition for the membrane potential
 
 
-spec.connections(1,1).label = 'soma-soma';
-spec.connections(1,1).mechanisms = {'soma_soma_iSYN'};
-spec.connections(1,1).parameters = [];
+spec.connections(1).direction = 'soma->soma';
+spec.connections(1).mechanism_list = {'soma_soma_iSYN'};
+spec.connections(1).parameters = [];
 
-spec.connections(1,2).label = 'soma-dend';
-spec.connections(1,2).mechanisms = {'soma_dend_iCOM'};
-spec.connections(1,2).parameters = [];
+spec.connections(2).direction = 'soma->dend';
+spec.connections(2).mechanism_list = {'soma_dend_iCOM'};
+spec.connections(2).parameters = [];
 
-spec.connections(2,1).label = 'dend-soma';
-spec.connections(2,1).mechanisms = {'dend_soma_iCOM'};
-spec.connections(2,1).parameters = [];
+spec.connections(3).direction = 'dend->soma';
+spec.connections(3).mechanism_list = {'dend_soma_iCOM'};
+spec.connections(3).parameters = [];
 
-spec.connections(2,2).label = 'dend-dend';
-spec.connections(2,2).mechanisms = {'dend_dend_iGAP'};
-spec.connections(2,2).parameters = [];
+spec.connections(4).direction = 'dend->dend';
+spec.connections(4).mechanism_list = {'dend_dend_iGAP'};
+spec.connections(4).parameters = [];
 
-spec.connections(3,3).label = [spec.nodes(3).label,'-',spec.nodes(3).label];
-spec.connections(3,3).mechanisms = {'gabaRecInputMSN'};
-spec.connections(3,3).parameters = {'g_gaba',g_gaba};
+spec.connections(5).direction = [spec.nodes(3).name,'->',spec.nodes(3).name];
+spec.connections(5).mechanism_list = {'gabaRecInputMSN'};
+spec.connections(5).parameters = {'g_gaba',g_gaba};
 
-spec.connections(4,4).label = [spec.nodes(4).label,'-',spec.nodes(4).label];
-spec.connections(4,4).mechanisms = {'gabaRecInputMSN'};
-spec.connections(4,4).parameters = {'g_gaba',g_gaba};
+spec.connections(6).direction = [spec.nodes(4).name,'->',spec.nodes(4).name];
+spec.connections(6).mechanism_list = {'gabaRecInputMSN'};
+spec.connections(6).parameters = {'g_gaba',g_gaba};
 
-spec.connections(1,3).label = 'soma-D1';
-spec.connections(1,3).mechanisms = {'soma_MSN_iSYN'};
-spec.connections(1,3).parameters = {'gsyn',6*g_gaba};
+spec.connections(7).direction = 'soma->D1';
+spec.connections(7).mechanism_list = {'soma_MSN_iSYN'};
+spec.connections(7).parameters = {'gsyn',6*g_gaba};
 
-spec.connections(1,4).label = 'soma-D2';
-spec.connections(1,4).mechanisms = {'soma_MSN_iSYN'};
-spec.connections(1,4).parameters = {'gsyn',6*g_gaba};
+spec.connections(8).direction = 'soma->D2';
+spec.connections(8).mechanism_list = {'soma_MSN_iSYN'};
+spec.connections(8).parameters = {'gsyn',6*g_gaba};
 
 %dnsim(spec); % open model in DNSim GUI
 
@@ -167,6 +172,13 @@ scope = {'(dend,dend-dend,soma-soma,D1,D2)','(soma-D1,soma-D2)','(D1,D2)','(soma
 variable = {'DA','gsyn','g_m','taub'};
 values = {'[0:1]','[0.01]','[1.3]','[120]'};
 
+vary={
+  '(dend,dend-dend,soma-soma,D1,D2)',	'DA',	[0:1];
+  '(soma-D1,soma-D2)',           		'gsyn',	[0.01];
+  '(D1,D2)',   							'g_m',	[1.3];
+  '(soma,dend)',						'taub', [120];
+};
+
 %scope = {'(dend,dend-dend,soma-soma)','(soma-D1,soma-D2)','(D1,D2)','(D1,D2)'};
 %variable = {'DA','gsyn','g_m','injectedCurrent'};
 %values = {'[0:0.1:1]','[0.01]','[1.2,1.3]','[1.1:0.01:1.3]'};
@@ -177,8 +189,32 @@ values = {'[0:1]','[0.01]','[1.3]','[120]'};
 % variable = {'DA','tonic','tauD'}
 % values = {'[0,1]','[0:20]','[9:13]'}
 
-[~,~]=SimulateModel(spec,scope,variable,values,...
-  'dt',.01,'SOLVER','rk4', 'memlimit', '64G', 'overwrite_flag', 1, 'timelimits',[0 T0], 'rootdir', '/projectnb/crc-nak/chartove/dnsim', ...
-  'savedata_flag',1,'timesurfer_flag',0,'saveplot_flag',0,'plotvars_flag',0,'addpath','/project/crc-nak/jchartove/dnsim',...
-  'cluster_flag',1);
+memlimit = '64G';
+data_dir = '/projectnb/crc-nak/chartove/dnsim/';
+cluster_flag = 0;
+overwrite_flag = 1;
+save_data_flag = 1;
+% Even if `save_data_flag` is 0, if running on cluster this must be off too in
+%   order to not save data?
+save_results_flag = 1;
+verbose_flag = 1;
+compile_flag = 0;
+disk_flag = 0;
+downsample_factor = 10;
+
+% local run of the simulation,
+%   i.e. in the interactive session you're running this same script in
+[~,~]=SimulateModel(spec,'save_data_flag',save_data_flag,'study_dir',data_dir,...
+              'cluster_flag',cluster_flag,'verbose_flag',verbose_flag,...
+              'overwrite_flag',overwrite_flag,'tspan',[0 T0],...
+              'save_results_flag',save_results_flag,'solver','rk4',...
+              'memlimit',memlimit,'compile_flag',compile_flag,...
+              'disk_flag',disk_flag,'downsample_factor',downsample_factor,...
+              'vary',vary, 'dt', .01, ...
+              'plot_functions',{@PlotData,@PlotData},...
+              'plot_options',{{'plot_type','waveform','format','png'},...
+                              {'plot_type','power','format','png',...
+                               'xlim',[0 40]}});
+
 end
+			  %'addpath','/project/crc-nak/jchartove/dnsim',...
